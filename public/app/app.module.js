6ac771b1e2e97654ae040d8fa3edc0cc0b877baa
Jeroen Valcke <valcke_jeroen@hotmail.com>
2018-05-18 16:11:54 +0200

    0.0.10

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,19 +1,19 @@
 "use strict";
 
 (function(angular) {
-	angular.module("wcm-participation_0.0.9.factories", []);
-	angular.module("wcm-participation_0.0.9.services", ["wcm-participation_0.0.9.factories"]);
-	angular.module("wcm-participation_0.0.9.controllers", ["wcm-participation_0.0.9.services"]);
-	angular.module("wcm-participation_0.0.9.directives", ["wcm-participation_0.0.9.controllers"]);
+	angular.module("wcm-participation_0.0.10.factories", []);
+	angular.module("wcm-participation_0.0.10.services", ["wcm-participation_0.0.10.factories"]);
+	angular.module("wcm-participation_0.0.10.controllers", ["wcm-participation_0.0.10.services"]);
+	angular.module("wcm-participation_0.0.10.directives", ["wcm-participation_0.0.10.controllers"]);
 
-	angular.module("wcm-participation_0.0.9", [
+	angular.module("wcm-participation_0.0.10", [
 
 		"pelorus.services",
 
-		"wcm-participation_0.0.9.factories",
-		"wcm-participation_0.0.9.services",
-		"wcm-participation_0.0.9.controllers",
-		"wcm-participation_0.0.9.directives",
+		"wcm-participation_0.0.10.factories",
+		"wcm-participation_0.0.10.services",
+		"wcm-participation_0.0.10.controllers",
+		"wcm-participation_0.0.10.directives",
 
 	])
 		.run([function() {
--- public/app/providers/participations-config.provider.js
+++ public/app/providers/participations-config.provider.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (function(angular) {
-	angular.module("wcm-participation_0.0.9")
+	angular.module("wcm-participation_0.0.10")
 		.provider("participationsConfig", [
 
 			"MODULE_ENV_CONFIG",
@@ -10,7 +10,7 @@
 
 				this.API = {
 					name: MODULE_ENV_CONFIG.angularModule,
-					version: "0.0.9",
+					version: "0.0.10",
 					feDirPath: MODULE_ENV_CONFIG.feDirPath,
 					assetsDirPath: MODULE_ENV_CONFIG.assetsDirPath,
 					cssDirPath: MODULE_ENV_CONFIG.cssDirPath,
