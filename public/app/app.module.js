1ce6c5930ee734fb1cf2f1bffdcebfc025cd5ad8
Jeroen Valcke <valcke_jeroen@hotmail.com>
2018-05-24 12:29:32 +0200

    0.0.12

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,19 +1,19 @@
 "use strict";
 
 (function(angular) {
-	angular.module("wcm-participation_0.0.11.factories", []);
-	angular.module("wcm-participation_0.0.11.services", ["wcm-participation_0.0.11.factories"]);
-	angular.module("wcm-participation_0.0.11.controllers", ["wcm-participation_0.0.11.services"]);
-	angular.module("wcm-participation_0.0.11.directives", ["wcm-participation_0.0.11.controllers"]);
+	angular.module("wcm-participation_0.0.12.factories", []);
+	angular.module("wcm-participation_0.0.12.services", ["wcm-participation_0.0.12.factories"]);
+	angular.module("wcm-participation_0.0.12.controllers", ["wcm-participation_0.0.12.services"]);
+	angular.module("wcm-participation_0.0.12.directives", ["wcm-participation_0.0.12.controllers"]);
 
-	angular.module("wcm-participation_0.0.11", [
+	angular.module("wcm-participation_0.0.12", [
 
 		"pelorus.services",
 
-		"wcm-participation_0.0.11.factories",
-		"wcm-participation_0.0.11.services",
-		"wcm-participation_0.0.11.controllers",
-		"wcm-participation_0.0.11.directives",
+		"wcm-participation_0.0.12.factories",
+		"wcm-participation_0.0.12.services",
+		"wcm-participation_0.0.12.controllers",
+		"wcm-participation_0.0.12.directives",
 
 	])
 		.run([function() {
