32ae3fef334b5cacf866bbb8b633639bcc91409b
Denis Valcke <denis.valcke@gmail.com>
2020-12-08 11:57:00 +0100

    fix: 1.0.15

--- public/app/factories/participations.provider.js
+++ public/app/factories/participations.provider.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (function(angular) {
-	angular.module("wcm-participation_1.0.0.factories")
+	angular.module("wcm-participation_1.0.15.factories")
 		.factory("participationsFactory", [
 
 			"$resource",
